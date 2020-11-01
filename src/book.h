43798fc792b3d53aebf06fa63b86b0b4e1cdc31b
Magnus Viktorsson <m.viktorsson@gmail.com>
2020-11-01 22:50:40 +0100

    small changes

--- src/book.h
+++ src/book.h
@@ -5,11 +5,11 @@
 class Book
 {
 public:
-    Book(std::string isbn, std::string title, std::string author, std::string copyright_date) : ISBN_(verifyISBN(isbn)), title_(title), author_(author), copyright_date_(copyright_date){};
-    std::string getISBN() { return ISBN_; };
-    std::string getTitle() { return title_; };
-    std::string getAuthor() { return author_; };
-    std::string getCopyright_date() { return copyright_date_; };
+    Book(std::string isbn, std::string title, std::string author, std::string copyright_date) : ISBN_(verifyISBN(isbn)), title_(title), author_(author), copyright_date_(copyright_date), isCheckedOut_(false){};
+    std::string getISBN() const { return ISBN_; };
+    std::string getTitle() const { return title_; };
+    std::string getAuthor() const { return author_; };
+    std::string getCopyright_date() const { return copyright_date_; };
     void checkInBook();
     void checkOutBook();
 
@@ -20,10 +20,10 @@ private:
     std::string copyright_date_;
     bool isCheckedOut_;
 
-    static std::string verifyISBN(const std::string &isbn)
+    std::string verifyISBN(const std::string &isbn)
     {
         // Simple regex for validating example ISBN to be in form n-n-n-x where n is integer and x is digit / or letter.
-        std::regex simpleISBNregex{"(\\d-\\d-\\d-\\w|\\d)"};
+        const std::regex simpleISBNregex{"(\\d-\\d-\\d-\\w|\\d)"};
         if (!std::regex_match(isbn, simpleISBNregex))
         {
             throw std::invalid_argument("Invalid ISBN code.");
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,5 @@ int main()
     std::cout << "Welcome the the library \n";
 
     Book test{"5-4-5-x", "Flugornas Herre", "Anders Andersson", "2017-12-12"};
-
     return 0;
 }
